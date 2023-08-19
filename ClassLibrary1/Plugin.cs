dbb5d7cfa1a7c91f6a907d1412f2351a0a41a600
yjjx <rusil.versh@gmail.com>
2023-08-19 16:37:38 +0300

    1

--- ClassLibrary1/Plugin.cs
+++ ClassLibrary1/Plugin.cs
@@ -1,7 +1,10 @@
-﻿using CommandSystem.Commands.RemoteAdmin.ServerEvent;
+﻿using CommandSystem.Commands.RemoteAdmin;
+using CommandSystem.Commands.RemoteAdmin.ServerEvent;
 using Exiled.API.Features;
 using Exiled.Events.EventArgs.Player;
 using Exiled.Events.EventArgs.Server;
+using InventorySystem;
+using PlayerRoles;
 using PluginAPI.Events;
 using System.Diagnostics.Eventing.Reader;
 
@@ -24,7 +27,49 @@ namespace ClassLibrary1
             base.OnEnabled();
             Exiled.Events.Handlers.Player.ActivatingGenerator += Player_ActivatingGenerator;
             Exiled.Events.Handlers.Player.Shooting += Player_Shooting;
+            Exiled.Events.Handlers.Player.Spawned += Player_Spawned1;
+            Exiled.Events.Handlers.Warhead.Detonated += Warhead_Detonated;
 
+        }
+
+        private void Player_Spawned1(SpawnedEventArgs ev)
+        {
+            //throw new System.NotImplementedException();
+            if (ev.Player.Role.Type is RoleTypeId.Scientist)
+            {
+                ev.Player.Health = 500;
+                ev.Player.Inventory.ServerAddItem(ItemType.SCP500);
+                ev.Player.Inventory.ServerAddAmmo(ItemType.Ammo9x19, 100);
+
+
+            }
+        }
+
+        private void Warhead_Detonated()
+        {
+            //throw new System.NotImplementedException();
+            foreach (Exiled.API.Features.Player Player in Exiled.API.Features.Player.List)
+            {
+                Player.Kill("у вас сгорело очко");
+            }
+        }
+
+        private void Player_ChangingRole(ChangingRoleEventArgs ev)
+        {
+            //throw new System.NotImplementedException();
+            if ( ev.NewRole is RoleTypeId.Scientist)
+            {
+                ev.Player.Health = 500;
+                ev.Player.Inventory.ServerAddItem(ItemType.SCP500);
+                ev.Player.Inventory.ServerAddAmmo(ItemType.Ammo9x19, 100);
+     
+
+            }
+        }
+
+        private void Player_Spawned(SpawnedEventArgs ev)
+        {
+            //throw new System.NotImplementedException();
         }
 
         private void Player_Shooting(ShootingEventArgs ev)
