0f249cbbe209ac89e202c7f483e89a3f06c84974
yjjx <rusil.versh@gmail.com>
2023-08-19 15:07:50 +0300

    timer

--- ClassLibrary1/kal.cs
+++ ClassLibrary1/kal.cs
@@ -5,8 +5,10 @@ using Exiled.API.Features;
 using MEC;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ClassLibrary1
@@ -14,12 +16,12 @@ namespace ClassLibrary1
     [CommandHandler(typeof(ClientCommandHandler))]
     internal class kal : ICommand
     {
-        public IEnumerator<float> MyCoroutine()
+        public IEnumerator<float> MyCoroutine(int x)
         {
             for (; ; )
             {
                 Log.Info("kek");
-                yield return Timing.WaitForSeconds(5f);
+                yield return Timing.WaitForSeconds(x);
             }
         }
         public string Command => "pizda";
@@ -29,12 +31,23 @@ namespace ClassLibrary1
 
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
+            var time = arguments;
+
+            if (arguments.Count != 1)
+            {
+                response = "введите длительность";
+                return false;
+            }
+            if (int.TryParse(arguments.At(0), out int value))
+            {
+                Timing.RunCoroutine(MyCoroutine(value));
+                response = "ЖДЕМ СУКА";
+            }
             //throw new NotImplementedException();
             var PlayerGet = Player.Get(sender);
             var room = PlayerGet.CurrentRoom;
             room.TurnOffLights(5);
             PlayerGet.Broadcast(5, Plugin.Configs.testa);
-            Timing.RunCoroutine(MyCoroutine());
             room.LockDown(5);
             response = "sveta net";
             return true;
